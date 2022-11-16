bc7a678b8ca6c35dbbd91129d2365fc1bde7e23a
kevin-zx <zhangxian887@qq.com>
2022-11-16 14:23:12 +0800

    add FileJsonSave

--- kjson/json.go
+++ kjson/json.go
@@ -13,3 +13,12 @@ func FileJsonLoad(filename string, v interface{}) error {
 	defer f.Close()
 	return json.NewDecoder(f).Decode(v)
 }
+
+func FileJsonSave(filename string, v interface{}) error {
+	f, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	return json.NewEncoder(f).Encode(v)
+}
